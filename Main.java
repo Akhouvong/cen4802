8c20cf5495f3c53b9049d18e0af62260b0c9666b
Akhouvong <84209107+Akhouvong@users.noreply.github.com>
2022-05-17 20:07:43 -0400

    Update Main.java
    Added javadoc comments.

--- Main.java
+++ Main.java
@@ -1,16 +1,27 @@
 import java.util.Scanner;
 public class Main {
+	/**
+	* Get input from the user of desired nth term of the Fibonacci sequence and then desplays desired nth term of the Fibonacci sequence.
+	*
+	*@author Alex Khouvongsavanh
+	*/
 	public static void main(String[] args) {
-		int n;
+		long n;
 		
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Please enter nth term of the Fibonacci sequence: ");
-		n = input.nextInt();
+		n = input.nextLong();
 		System.out.println("The nth term of the fibonacci sequence is: " + fib(n));
 	}
 	
-	public static int fib(int n) {
+	/**
+	*calculates the desired nth term of the fibonacci sequence, given the data provided.
+	*
+	* @param fib 
+	*@return nth term of the fibonacci sequence
+	*/
+	public static long fib(long n) {
 	      if ((n == 0) || (n == 1))
 	    	  return n;
 	      else
